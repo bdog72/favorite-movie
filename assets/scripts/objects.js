f1bfc32d6cfbd2137336c9f43ad687a43a217610
Brian Bycynski <high5bri@hotmail.com>
2019-11-11 04:06:01 -0500

    finished

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,6 +3,39 @@ const searchButton = document.querySelector('#search-btn');
 
 const movies = [];
 
+const renderMovies = (filter = '') => {
+  const movieList = document.querySelector('#movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
+    const movieElement = document.createElement('li');
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    // const { title: movieTitle } = info;
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = `${getFormattedTitle.apply(movie)} - `;
+    for (const key in info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${info[key]}`;
+      }
+    }
+    movieElement.textContent = text;
+    movieList.append(movieElement);
+  });
+};
+
 const addMovieHandler = () => {
   const title = document.querySelector('#title').value;
   const extraName = document.querySelector('#extra-name').value;
@@ -21,10 +54,20 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue
     },
-    id: Math.random()
+    id: Math.random().toString(),
+    getFormattedTitle() {
+      console.log(this);
+      return this.info.title.toUpperCase();
+    }
   };
   movies.push(newMovie);
-  console.log(newMovie);
+  renderMovies();
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.querySelector('#filter-title').value;
+  renderMovies(filterTerm);
 };
 
 addMovieButton.addEventListener('click', addMovieHandler);
+searchButton.addEventListener('click', searchMovieHandler);
